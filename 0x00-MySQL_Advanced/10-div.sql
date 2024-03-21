bd11fa50e2000725961d2d454656ff69b264a8cd
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-03-21 23:30:41 +0300

    create view

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,19 @@
+-- SQL script that creates a function SafeDiv that divides (and returns) the first by the second number or returns 0 if the second number is equal to 0.
+DELIMITER //
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS DECIMAL(10,4)
+BEGIN
+    DECLARE result DECIMAL(10,4);
+    
+    IF b = 0 THEN
+        SET result = 0;
+    ELSE
+        SET result = a / b;
+    END IF;
+
+    RETURN result;
+END;
+//
+
+DELIMITER ;
