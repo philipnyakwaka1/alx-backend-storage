7f53cf3bfebc3afdec32f66925389d0c3d8a01e0
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-03-26 18:44:18 +0300

    lists all documents in a collection

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+This module defines  function that changes all topics of
+a school document based on the name
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    This function that changes all topics of a school
+    document based on the name
+    """
+    return mongo_collection.update_many({'name': name}, {'$set': {
+        'topics': topics
+    }})
