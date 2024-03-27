9d143ddd9582c3d6306d7ef645ca806f632d322a
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-03-27 16:38:59 +0300

    create Cache class

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -12,14 +12,15 @@ class Cache:
     Definantion of class Cache
     """
     def __init__(self) -> None:
-        """
-        Store an instance of the Redis client as a private variable and
-        flushes the redis database
-        """
+        """Initializes a Cache object"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[int, float, str, bytes]) -> str:
+        """
+        Store an instance of the Redis client as a private variable and
+        flushes the redis database
+        """
         id = str(uuid.uuid4())
         self._redis.set(id, data)
         return id
